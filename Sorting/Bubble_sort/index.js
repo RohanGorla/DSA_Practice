da491dec55034efd708bb5bd5a41810ac9831a55
RohanGorla <rohangorla2010@gmail.com>
2025-03-12 19:08:29 +0530

    Add a check swapped variable to optimize the bubble sort algorithm to break out of the loop once the array is sorted

--- Sorting/Bubble_sort/index.js
+++ Sorting/Bubble_sort/index.js
@@ -3,14 +3,17 @@ const start = performance.now();
 
 // Function to sort using bubble sort algorithm
 function bubbleSort(arr) {
+  let swapped = false;
   for (let i = 0; i < arr.length - 1; i++) {
     for (let j = 0; j < arr.length - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         const elementsToSwap = [arr[j], arr[j + 1]];
         arr[j] = elementsToSwap[1];
         arr[j + 1] = elementsToSwap[0];
+        swapped = true;
       }
     }
+    if (!swapped) break;
   }
   // Return sorted array
   return arr;
@@ -23,11 +26,11 @@ const arrayToSort = [36, 10, 5, 90, 54, 78, 12, 15, 10];
 const sortedArray = bubbleSort(arrayToSort);
 // Timer end and time calculation
 const end = performance.now();
-const timeTaken = (end - start).toFixed(4);
+const timeTaken = end - start;
 
 console.log(
   "Sorted array -> ",
   sortedArray,
   "\n",
-  `Time taken -> ${timeTaken}ms || ${timeTaken / 1000}s`
+  `Time taken -> ${timeTaken.toFixed(6)}ms || ${(timeTaken / 1000).toFixed(6)}s`
 );
